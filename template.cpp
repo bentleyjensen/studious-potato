baf722c006335e8cd42a6f58ebc7cc8da08322e8
Bentley Jensen <bentleyjensen@users.noreply.github.com>
2018-04-27 17:54:15 -0700

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,90 @@
+/******************************************************************************
+* Program:
+*    Assignment 40, asdf
+*    Sister Hansen, CS 124
+* Author:
+*    Sterling Jensen
+* Summary:
+*    asdf
+*
+*    Estimated:  asdf Minutes
+*    Actual:     asdf Minutes
+*
+*    Please describe briefly the most difficult part.
+*     asdf
+*
+******************************************************************************/
+
+// Libraries
+#include <iostream>  // I/O Streams (cin, cout)
+#include <iomanip>   // IO Manipulator (setw)
+#include <fstream>   // File streams (ofstream, ifstream)
+#include <string>    // String Class
+#include <cassert>   // Error Handling (assert)
+using namespace std;
+
+
+//Prototypes
+void asdf(int asdf);
+
+
+
+/******************************************************************************
+ * FUNCTION: MAIN
+ * Input:
+ *    list:    a list of numbers to search through
+ *    max:     the size of the numbers list
+ * Output:
+ *    num:     the number of items actually read
+ * asdf
+ *****************************************************************************/
+int main()
+{
+
+   return 0;
+}
+
+/******************************************************************************
+ * FUNCTION: ASDF
+ *    asdf
+ * Input:
+ *    asdf:    asdf
+ * Output:
+ *    asdf:    asdf
+ *****************************************************************************/
+void asdf (int asdf)
+{
+
+   return;
+}
+
+
+/******************************************************************************
+ * FUNCTION: ASDF
+ *    asdf
+ * Input:
+ *    asdf:    asdf
+ * Output:
+ *    asdf:    asdf
+ *****************************************************************************/
+void asdf (int asdf)
+{
+
+   return;
+}
+
+
+/******************************************************************************
+ * FUNCTION: ASDF
+ *    asdf
+ * Input:
+ *    asdf:    asdf
+ * Output:
+ *    asdf:    asdf
+ *****************************************************************************/
+void asdf (int asdf)
+{
+
+   return;
+}
+
